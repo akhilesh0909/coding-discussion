0e771094ebacb4c5184d9ffc630672a408033605
T0260265 <bhatt.akhilesh@thalesgroup.com>
2023-09-13 10:46:40 +0530

    Etend expiry time of token

--- src/main/java/com/ems/codingdiscussion/utils/JWTUtil.java
+++ src/main/java/com/ems/codingdiscussion/utils/JWTUtil.java
@@ -43,7 +43,7 @@ public class JWTUtil {
 	    private String createToken(Map<String, Object> claims, String subject) throws Exception {
 
 	        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-	                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
+	                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 100))
 	                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
 	    }
 
