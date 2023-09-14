f6d886cd86e32e0cae4adf624056d020a77aab69
Anika Kalia T0263689 <anika.kalia@thalesgroup.com>
2023-09-14 16:32:56 +0530

    Updated Jwt authentication

--- src/main/java/com/ems/codingdiscussion/controllers/AuthenticationController.java
+++ src/main/java/com/ems/codingdiscussion/controllers/AuthenticationController.java
@@ -1,9 +1,13 @@
 package com.ems.codingdiscussion.controllers;
 
+import java.util.Optional;
+
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,7 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ems.codingdiscussion.dtos.AuthDTO;
 import com.ems.codingdiscussion.dtos.LoginDTO;
+import com.ems.codingdiscussion.entities.User;
+import com.ems.codingdiscussion.repositories.UserRepository;
 import com.ems.codingdiscussion.utils.JWTUtil;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class AuthenticationController {
@@ -25,26 +33,42 @@ public class AuthenticationController {
 	private UserDetailsService userDetailsService;
 	
 	@Autowired
+	private UserRepository userRepository;	
+	
+	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	public static final String TOKEN_PREFIX ="Bearer ";
+	public static final String HEADER_STRING ="Authorization";
 	
 	@PostMapping("/authenticate")
-	public ResponseEntity<?> createJwtToken(@RequestBody LoginDTO loginDTO) throws Exception{
+	public void createJwtToken(@RequestBody LoginDTO loginDTO, HttpServletResponse response) throws Exception,DisabledException,BadCredentialsException{
 		
 		try {
 		authenticationManager.authenticate(
 				new UsernamePasswordAuthenticationToken(loginDTO.getEmail(),loginDTO.getPassword()));
 		}
 		catch(BadCredentialsException e){
-			throw new Exception("Email or Password not valid", e);
-			
+	    	throw new BadCredentialsException("Email or Password not valid");	
+		}
+		catch(DisabledException disabledException) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND,"USER IS NOT CREATED");
+			return;
 		}
 		
 		final UserDetails userDetails = userDetailsService.loadUserByUsername(loginDTO.getEmail());
 		
+		Optional<User> optionalUser = userRepository.findFirstByEmail(userDetails.getUsername());
 		final String jwtToken = jwtUtil.generateToken(userDetails);
 		
-		return ResponseEntity.ok(new AuthDTO(jwtToken));
+		if(optionalUser.isPresent()) {
+			response.getWriter().write(new JSONObject().put("userId", optionalUser.get().getId()).toString());
+		}
+		response.addHeader("Access-Control-Exppose-Headers", "Authorization");
+		response.setHeader("Access-Control-Allow-Headers", "Authorization,X-PINGOTHER,X-Requested-With,Content-Type, Accept,X-Custom-header");
+		
+		response.setHeader(HEADER_STRING, TOKEN_PREFIX +jwtToken);
+		
 	}
 	
 	
