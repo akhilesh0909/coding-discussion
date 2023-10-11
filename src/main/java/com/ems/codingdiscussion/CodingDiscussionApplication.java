88366498c7c053315b25dde6651eb2706f06dbd0
T0260265 <bhatt.akhilesh@thalesgroup.com>
2023-10-11 15:41:38 +0530

    commit for build

--- src/main/java/com/ems/codingdiscussion/CodingDiscussionApplication.java
+++ src/main/java/com/ems/codingdiscussion/CodingDiscussionApplication.java
@@ -2,9 +2,10 @@ package com.ems.codingdiscussion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CodingDiscussionApplication {
+public class CodingDiscussionApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodingDiscussionApplication.class, args);
--- src/main/java/com/ems/codingdiscussion/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/ems/codingdiscussion/configuration/WebSecurityConfiguration.java
@@ -1,43 +1,87 @@
-//package com.ems.codingdiscussion.configuration;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableMethodSecurity
-//@EnableWebSecurity
-//public class WebSecurityConfiguration {
-//	
+package com.ems.codingdiscussion.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import com.ems.codingdiscussion.filters.JWTRequestFilter;
+import com.ems.codingdiscussion.services.UserDetailServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfiguration {
+	
+	@Autowired
+	private JWTRequestFilter jwtRequestFilter;
+	
+	@Autowired
+	private UserDetailServiceImpl userDetailServiceImpl;
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setPasswordEncoder(encoder());
+		daoAuthenticationProvider.setUserDetailsService(userDetailServiceImpl);
+		return daoAuthenticationProvider;
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+		
+	}
+	
 //	@Bean
-//	public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
-//		return security
-//				.csrf()
-//				.disable()
-//				.authorizeHttpRequests()
-//				.requestMatchers("/sign-up")
-//				.permitAll()
-//				.and()
-//				.authorizeHttpRequests()
-//				.requestMatchers("")
-//				.authenticated()
-//				.and()
-//				.sessionManagement()
-//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//				.and()
-//				.build();
-//		
-//	}
-//	
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-//
-//}
+//    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+//        return new MvcRequestMatcher.Builder(introspector);
+//    } 
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+		return security
+				.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+				.csrf()
+				.disable()
+				.authorizeHttpRequests()
+				.requestMatchers(AntPathRequestMatcher.antMatcher("/sign-up"),AntPathRequestMatcher.antMatcher("/authenticate"))
+				.permitAll()
+				.and()
+				.authorizeHttpRequests()
+				.requestMatchers(AntPathRequestMatcher.antMatcher("/admin"),AntPathRequestMatcher.antMatcher("/api/**"),AntPathRequestMatcher.antMatcher("/user"),AntPathRequestMatcher.antMatcher("/"))
+				.authenticated()
+				.and()
+				.formLogin()
+				.and()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.build();
+		
+	}
+	
+	
+
+}
